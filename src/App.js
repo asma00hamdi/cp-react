df7a7bf0b59ee709a878e83b3e01edb40cbbad66
asma00hamdi <assoumahamdi052@gmail.com>
2023-05-18 14:28:59 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Formulaire from './components/Formulaire';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+  <div >
+  <Navbar />
+<Formulaire/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+<ul>
+<li>Home</li>
+<li>Contact</li>
+<li>About</li>
+
+
+</ul>
+
+
+    </div>
+  )
+}
+
+export default Navbar
